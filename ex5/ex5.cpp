e629fda53d95789b14e87cc90bd9155c601f2e4b
ariela <arielag1998@gmail.com>
2025-05-03 17:07:28 +0300

    ex5 complete

--- ex5/ex5.cpp
+++ ex5/ex5.cpp
@@ -1,41 +1,44 @@
-#include <iostream>
 #include "algorithm.h"
-
+#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
-    srand(42);
-    int n;
+int *generate_random_array(int n, int seed) {
+  srand(seed);
+  int *arr = new int[n];
+  for (int i = 0; i < n; ++i) {
+    arr[i] = rand() % 100 - 25; // [-25, 74]
+  }
+  return arr;
+}
 
-    cin >> n;
-    int* arr = new int[n];
+void run_algorithms(int *arr, int n) {
+  cout << "Running of O(n) max sub array sum: ";
+  max_subarray_sum_n(arr, n);
+  cout << endl;
 
+  cout << "Running of O(n^2) max sub array sum: ";
+  max_subarray_sum_n2(arr, n);
+  cout << endl;
 
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = rand() % 100 - 25;
-    }
+  cout << "Running of O(n^3) max sub array sum: ";
+  max_subarray_sum_n3(arr, n);
+  cout << endl;
+}
 
-    cout << "Array: ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    cerr << "Usage: " << argv[0] << " <seed> <size>" << endl;
+    return 1;
+  }
 
-    cout << "n: " << endl;
-    max_subarray_sum_n(arr, n);
-    cout << "n^2: " << endl;
-    max_subarray_sum_n2(arr, n);
-    cout << "n^3: " << endl;
-    max_subarray_sum_n3(arr, n);
-    
-  
+  int seed = atoi(argv[1]);
+  int n = atoi(argv[2]);
 
+  int *arr = generate_random_array(n, seed);
+  run_algorithms(arr, n);
 
-
-    delete[] arr;
-    return 0;
+  delete[] arr;
+  return 0;
 }
